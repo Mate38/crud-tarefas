f07bc7ce5f0ad6f46e1878306a97d9cfb038f41f
Mateus Cardoso <matecardoso38@gmail.com>
2021-01-13 21:57:39 -0300

    Corrção refreshDatabase TagTest

--- tests/Feature/TagTest.php
+++ tests/Feature/TagTest.php
@@ -4,12 +4,12 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TagTest extends TestCase
 {
+    use RefreshDatabase;
+    
     public function test_list()
     {
         $route = '/api/tag/list';
